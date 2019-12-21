deb1eaa1f97a680226dc6f1a3fc3886c455784cf
ankitXXVI <asharma19@ilg.cat>
2019-12-21 23:40:28 +0100

    1.1

--- Programa1/src/programa1/Calculadora.java
+++ Programa1/src/programa1/Calculadora.java
@@ -6,84 +6,158 @@ public class Calculadora {
 	
 	public static void main(String[] args) {
 		
-		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
 		double var1 = 0;
 		double var2 = 0;
-		double resultado = 1;
-		String operacion;
+		double result = 1;
+		String operation = "";
 		
-		System.out.println(" ____________________________________");
-		System.out.println("|************************************|");
-		System.out.println("|******Welcome to my Calculator!*****|");
-		System.out.println("|************************************|");
-		System.out.println("|____________________________________|");
+		System.out.println("************************************");
+		System.out.println("******Welcome to my Calculator!*****");
+		System.out.println("************************************");
 		System.out.println("");
-		System.out.println("*****What operation would you like to do?*****");
-		System.out.println("*******Available Operations:******");
-		System.out.println("Addition, Substraction, Multiplication, Division, Exponentiation and Factorial.");
-		operacion = in.nextLine();
-		System.out.println();
-		System.out.println();
-		System.out.println("*****Insert your first value*****");
-		if(in.hasNextDouble()) {
-			var1 = in.nextDouble();
-		} else {
-			System.out.println("*****Value not permitted!*****");
-		}
-		System.out.println();
-		System.out.println();
-		System.out.println("*****Insert your second value*****");
-		if(in.hasNextDouble()) {
-			var2 = in.nextDouble();		
-		} else {
-			System.out.println("*****Value not permitted!*****");
-		}
-		System.out.println();
-		System.out.println();
-
-		if("Addition".equalsIgnoreCase(operacion)) {
-			resultado = var1 + var2;
-			System.out.println("The result is: " + resultado);
-			
-		} else if ("Substraction".equalsIgnoreCase(operacion)) {
-			resultado = var1 - var2;
-			System.out.println("The result is: " + resultado);
-			
-		} else if ("Multiplication".equalsIgnoreCase(operacion)) {
-			resultado = var1 * var2;
-			System.out.println("The result is: " + resultado);
-			
-		} else if ("Division".equalsIgnoreCase(operacion)) {
-			resultado = var1 / var2;
-			System.out.println("The result is: " + resultado);
-			
-			
-		} else if ("Exponentiation".equalsIgnoreCase(operacion)){
-			for(int i = 1; i <= var2; i++) {
-				resultado = resultado * var1;
-			}
-			System.out.println("The result is: " + resultado);
+		System.out.println("Enter operation number:");
+		System.out.println("");
+		System.out.println("(1) Addition");
+		System.out.println("(2) Substraction");
+		System.out.println("(3) Multiplication");
+		System.out.println("(4) Division");
+		System.out.println("(5) Exponentiation");
+		System.out.println("(6) Factorial");
+		System.out.println("");
+		operation = in.nextLine();
+		System.out.println("");
 		
-			
-		} else if ("factorial".equalsIgnoreCase(operacion)) {
-			while (resultado != 0) {
-				resultado = resultado * var1;
-				var1--;
-					if(var1 == 0) {
-						System.out.println("The result is: " + resultado);
-					}
-			}
-	
-		} else {
+		if(!operation.equals("1") && !operation.equals("2") && !operation.equals("3") && !operation.equals("4") && !operation.equals("5") && !operation.equals("6")) {
 			System.out.println("ERROR: Operation not permitted!");
-
+			System.out.println("");
+			System.out.println("Press <Enter> to try again");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				main(args);
+			}
 		}
 		
+		else if(operation.equals("6")) {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your value:");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					main(args);
+				}
+			}
+			while (result != 0) {
+				result = result * var1;
+				var1--;
+				if (var1 == 0) {
+					System.out.println("");
+					System.out.println("The result is: " + result);
+				}
+			}
+			System.out.println("");
+			System.out.println("Press <Enter> to perform another operation");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				main(args);
+			}
+		}
+		
+		else if(operation.equals("5")) {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your base value:");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Insert you exponent value:");
+			if(in.hasNextDouble()) {
+				var2 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			for(int i = 1; i <= var2; i++) {
+				result = result * var1;
+			}
+			System.out.println("The result is: " + result);
+			System.out.println("");
+			System.out.println("Press <Enter> to perform another operation");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				main(args);
+			}
+		}
+		
+		else {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your first value");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("*****Value not permitted!*****");
+			}
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Insert your second value:");
+			if(in.hasNextDouble()) {
+				var2 = in.nextDouble();
+			} else {
+				System.out.println("*****Value not permitted!*****");
+			}
+			System.out.println("");
+			System.out.println("");
+			
+			if(operation.equals("1")) {
+				result = var1 + var2;
+				System.out.println("The result is: " + result);	
+			}
+			
+			else if(operation.equals("2")) {
+				result = var1 - var2;
+				System.out.println("The result is: " + result);
+			}
+			
+			else if(operation.equals("3")) {
+				result = var1 * var2;
+				System.out.println("The result is: " + result);
+			}
+			
+			else if(operation.equals("4")) {
+				result = var1 / var2;
+				System.out.println("The result is: " + result);
+			}
+		}
+		
+		System.out.println("");
 		System.out.println("Press <Enter> to perform another operation");
 		if(in.nextLine() != null) {
 			in.nextLine();
 			main(args);
-		}
+		}	
 	}
 }
