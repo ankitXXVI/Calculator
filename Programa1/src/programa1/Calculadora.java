50294b46cc7bf05484651153af3e31feece2d30e
ankitXXVI <asharma19@ilg.cat>
2019-12-22 02:26:15 +0100

    1.3

--- Programa1/src/programa1/Calculadora.java
+++ Programa1/src/programa1/Calculadora.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Calculadora {
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
@@ -24,11 +25,18 @@ public class Calculadora {
 		System.out.println("(4) Division");
 		System.out.println("(5) Exponentiation");
 		System.out.println("(6) Factorial");
+		System.out.println("(7) Square root");
+		System.out.println("(8) Percentage value of a number");
+		System.out.println("(9) Percentage substraction from a number");
+		System.out.println("(10) Percentage addition to a number");
 		System.out.println("");
 		operation = in.nextLine();
 		System.out.println("");
 		
-		if(!operation.equals("1") && !operation.equals("2") && !operation.equals("3") && !operation.equals("4") && !operation.equals("5") && !operation.equals("6")) {
+		if(!operation.equals("1") && !operation.equals("2") && !operation.equals("3")
+		&& !operation.equals("4") && !operation.equals("5") && !operation.equals("6")
+		&& !operation.equals("7") && !operation.equals("8") && !operation.equals("9")
+		&& !operation.equals("10")) {
 			System.out.println("ERROR: Operation not permitted!");
 			System.out.println("");
 			System.out.println("Press <Enter> to try again");
@@ -110,6 +118,170 @@ public class Calculadora {
 				result = result * var1;
 			}
 			System.out.println("The result is: " + result);
+			System.out.println("");
+			System.out.println("Press <Enter> to perform another operation");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				clearScreen();
+				main(args);
+			}
+		}
+		
+		else if(operation.equals("7")) {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your base value:");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			result = Math.sqrt(var1);
+			System.out.println("The result is " + result);
+			System.out.println();
+			System.out.println("Press <Enter> to perform another operation");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				clearScreen();
+				main(args);
+			}
+		}
+		
+		else if(operation.equals("8")) {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your value:");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Insert your percentage value:");
+			if(in.hasNextDouble()) {
+				var2 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			result = (var1 * var2) / 100;
+			System.out.println("The " + var2 + "% of " + var1 + " is " + result);
+			System.out.println("");
+			System.out.println("Press <Enter> to perform another operation");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				clearScreen();
+				main(args);
+			}
+		}
+		
+		else if(operation.equals("9")) {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your value:");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Insert your percentage value:");
+			if(in.hasNextDouble()) {
+				var2 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			result = var1 - ((var1 * var2) / 100);
+			System.out.println(var1 + " - " + var2 + "%" + " is " + result);
+			System.out.println("");
+			System.out.println("Press <Enter> to perform another operation");
+			if(in.nextLine() != null) {
+				in.nextLine();
+				clearScreen();
+				main(args);
+			}
+		}
+		
+		else if(operation.equals("10")) {
+			System.out.println("NOTE: Use commas for decimal values.");
+			System.out.println("Insert your value:");
+			if(in.hasNextDouble()) {
+				var1 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Insert your percentage value:");
+			if(in.hasNextDouble()) {
+				var2 = in.nextDouble();
+			} else {
+				System.out.println("");
+				System.out.println("*****Value not permitted!*****");
+				System.out.println("");
+				System.out.println("Press <Enter> to try again");
+				if(in.nextLine() != null) {
+					in.nextLine();
+					clearScreen();
+					main(args);
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			result = var1 + ((var1 * var2) / 100);
+			System.out.println(var1 + " + " + var2 + "%" + " is " + result);
 			System.out.println("");
 			System.out.println("Press <Enter> to perform another operation");
 			if(in.nextLine() != null) {
